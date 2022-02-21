ac119c57495a984001b4158fdc0e184500725e74
candy-dong <704483696@qq.com>
2022-02-21 15:12:59 +0800

    自定义组件的样式 options styleIsolation

--- components/test/test.js
+++ components/test/test.js
@@ -1,5 +1,10 @@
 // components/test/test.js
 Component({
+    options:{
+        // styleIsolation: 'isolated'// 组件内外结构样式互不影响
+        // styleIsolation: 'apply-shared'//外界影响组件，组件不影响外界
+        styleIsolation: 'shared'//内外影响
+    },
     /**
      * 组件的属性列表
      */
