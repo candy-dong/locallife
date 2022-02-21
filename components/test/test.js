82c4251be0d10311caa032c3d69944e34080e8d6
candy-dong <704483696@qq.com>
2022-02-21 16:52:59 +0800

    数据监听器 observers

--- components/test/test.js
+++ components/test/test.js
@@ -25,13 +25,30 @@ Component({
      * 组件的初始数据
      */
     data: {
-
+        n1:0,
+        n2:0,
+        sum:0
     },
-
+    observers:{
+        'n1,n2':function(newN1,newN2){
+            this.setData({
+                sum: newN1 + newN2
+            })
+        }
+    },
     /**
      * 组件的方法列表
      */
     methods: {
-
+        addN1(){
+            this.setData({
+                n1:this.data.n1 + 1,
+            })
+        },
+        addN2(){
+            this.setData({
+                n2:this.data.n2 + 1
+            })
+        },
     }
 })
